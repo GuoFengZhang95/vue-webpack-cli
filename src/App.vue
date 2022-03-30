82de398c9b37560dee205bae16c602733a355b6d
zevan <1591787827@qq.com>
2022-03-30 11:02:09 +0800

    vue2-webpack4-cli(1.0)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    {{ awaitNum }}
-    <div class="box"></div>
-    <img src="@/assets/images/404.png" alt="" />
-    <img src="@/assets/images/logo.svg" alt="" />
-    <img src="@/assets/images/bg_jiexuqiu.svg" alt="" />
-    <router-view></router-view>
-    <div></div>
+    <h1>hello,world</h1>
   </div>
 </template>
 
@@ -14,27 +8,12 @@
 export default {
   name: 'App',
   data() {
-    return {
-      awaitNum: 0,
-      img: require('@/assets/images/404.png'),
-      dom: <div style="color:red">123</div>
-    }
+    return {}
   },
-  mounted() {
-    console.log(this.img)
-    console.log(this.dom)
-  },
+  mounted() {},
   methods: {},
 }
 </script>
 
 <style lang="less">
-.box {
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
-  box-shadow: 10px 10px red;
-  transform: translateX(30px);
-  display: flex;
-}
 </style>
