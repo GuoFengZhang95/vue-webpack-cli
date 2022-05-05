8c946e4287e5e73fbbc0cc51eab538ba1eb1f7ac
zevan <1591787827@qq.com>
2022-05-05 08:51:55 +0800

    优化

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,5 @@
-import { message } from 'ant-design-vue'
-// import router from '../router'
 
 import axios from 'axios'
-
-import { loginCheckJump } from '@/api/index.js'
 
 // ========== 创建axios实例 ==========
 const service = axios.create({
@@ -15,10 +11,7 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use(
   config => {
-    config.params = Object.assign(
-      {},
-      config.params ? config.params : { t: new Date().getTime() }
-    ) //兼容IE请求缓存
+    // console.log(config)
     return config
   },
   error => {
@@ -31,51 +24,14 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
   response => {
+    // console.log(response)
     const res = response.data
-    if (Reflect.has(res, 'success')) {
-      return phpResHandle(res)
-    } else {
-      return javaResHandle(res)
-    }
+    Promise.resolve(res)
   },
   error => {
-    message.error('网络异常', 2)
     console.log('error' + error)
     return Promise.reject(error)
   }
 )
 
-function request(opts) {
-  return service(opts)
-}
-
-function phpResHandle(res) {
-  if (res.success === false) {
-    // 登陆过期
-    if (res.msg === '登录超时,请重新登录') {
-      loginCheckJump()
-      return Promise.reject(res)
-    }
-    return res
-  } else {
-    return res
-  }
-}
-
-function javaResHandle(res) {
-  if (res.code !== '0') {
-    // 登陆过期
-    if (res.code === '100001') {
-      loginCheckJump()
-      return Promise.reject(res)
-    }
-    if (res.msg !== '暂无查看全部权限') {
-      message.error(res.msg, 2)
-    }
-    return res
-  } else {
-    return res
-  }
-}
-
-export default request
+export default service
